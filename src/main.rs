a1b7c25e5d0f947039b5e5b3f7fc395debe8ac66
you06 <you1474600@gmail.com>
2020-05-09 14:44:50 +0800

    add ping
    Signed-off-by: you06 <you1474600@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -12,22 +12,38 @@ use providers::slack::Slack;
 struct Opts {
     #[clap(short = "c", long = "config", default_value = "config.toml")]
     config: String,
+    #[clap(short = "p", long = "ping", default_value = "")]
+    ping: String,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let opts: Opts = Opts::parse();
     let conf = Config::new(opts.config).unwrap();
+
+    if opts.ping != "" {
+        let slack_client = Slack::new(conf.slack_token.clone());
+        let _ = slack_client
+            .send_message(conf.slack_channel.clone(), opts.ping)
+            .await?;
+        return Ok(());
+    }
+
     let mut report = "".to_owned();
+    let (mut has_issue, mut has_topic) = (false, false);
 
     let github_client = GitHub::new(conf.github_token.to_owned());
     let user = github_client.get_user_result().await;
     println!("Current user: {}", user.unwrap());
 
     let issues = github_client.get_opened_issues(conf.repos.clone()).await?;
-    report.push_str(&format!("{} no-reply issues in 3 days\n", issues.len())[..]);
-    for issue in issues {
-        report.push_str(&format!("{}\n", issue)[..]);
+    
+    if issues.len() != 0 {
+        has_issue = true;
+        report.push_str(&format!("{} no-reply issues in 3 days\n", issues.len())[..]);
+        for issue in issues {
+            report.push_str(&format!("{}\n", issue)[..]);
+        }
     }
 
     let discourse_client = Discourse::new(
@@ -38,16 +54,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .find_no_reply_topics_by_categories(conf.discourse_categories.clone())
         .await?;
 
-    report.push_str(&format!("\n\n{} no-reply topics in TUG\n", topics.len())[..]);
-    for topic in topics {
-        report.push_str(&format!("{}\n", topic)[..]);
+    if topics.len() != 0 {
+        has_topic = true;
+        report.push_str(&format!("\n\n{} no-reply topics in TUG\n", topics.len())[..]);
+        for topic in topics {
+            report.push_str(&format!("{}\n", topic)[..]);
+        }
     }
-
+        
     if conf.slack_token != "" && conf.slack_channel != "" {
-        let slack_client = Slack::new(conf.slack_token.clone());
-        let _ = slack_client
-            .send_message(conf.slack_channel.clone(), report)
-            .await?;
+        if has_issue || has_topic {
+            let slack_client = Slack::new(conf.slack_token.clone());
+            let _ = slack_client
+                .send_message(conf.slack_channel.clone(), report)
+                .await?;
+        }
     } else {
         println!("{}", report);
     }
